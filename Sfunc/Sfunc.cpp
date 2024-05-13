fc1c468d9e2fa81133f4364bc3cfe354568f621d
annisadiann <annisadian0412@gmail.com>
2024-05-13 17:28:08 +0700

    menambahkan prosedur void mahasiswa::printAll

--- Sfunc/Sfunc.cpp
+++ Sfunc/Sfunc.cpp
@@ -31,5 +31,12 @@ void mahasiswa::setID()
     id = ++nim;
 }
 
+void mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
 
 
