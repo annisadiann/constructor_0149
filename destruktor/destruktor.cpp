ed1f73116b59fd55526543c3a2a9104d89f858d9
annisadiann <annisadian0412@gmail.com>
2024-05-13 18:44:24 +0700

    mendefinisikan kelas dengan private member

---
+++ destruktor/destruktor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+    int* arr;
+    int panjang;
+public:
+    angka(int); //Constuktor
+    ~angka(); //Destruktor
+
+    void cetakData();
+    void isiData();
+};
+
+
+
