b0d79b64efb0debcfee7e2aa316c2710ff4bd627
annisadiann <annisadian0412@gmail.com>
2024-05-13 10:30:48 +0700

    Add project files.

---
+++ mbrStatik/mbrStatik.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
+};
+
+int mahasiswa::nim = 10;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
